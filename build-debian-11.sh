0f57d6d4b1c1ddfacb3843998f67bfb713b9c98a
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2026-02-02 06:25:15 -0600

    Correct formatting in build-debian-11.sh
    Fix indentation and formatting in build script.

--- build-debian-11.sh
+++ build-debian-11.sh
@@ -13,7 +13,7 @@ rm packages-microsoft-prod.deb
 sudo apt-get update; \
   sudo apt-get -y install dotnet-sdk-9.0 git cmake clang ninja-build build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5-dev libgmp-dev libc++-dev zlib1g-dev
 
-(cd src/Miningcore && \
+(cd src && \
 BUILDIR=${1:-../../build} && \
 echo "Building into $BUILDIR" && \
 dotnet build -o ../../build)
