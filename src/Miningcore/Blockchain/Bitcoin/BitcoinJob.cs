f6f584d300bca5e9f605466bec543af15767e510
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2026-02-03 17:23:11 -0600

    change Pool Name

--- src/Miningcore/Blockchain/Bitcoin/BitcoinJob.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinJob.cs
@@ -693,7 +693,7 @@ public class BitcoinJob
         JobId = jobId;
 
         var coinbaseString = !string.IsNullOrEmpty(cc.PaymentProcessing?.CoinbaseString) ?
-            cc.PaymentProcessing?.CoinbaseString.Trim() : "Miningcore";
+            cc.PaymentProcessing?.CoinbaseString.Trim() : "KriptoKyng Pool";
 
         scriptSigFinalBytes = new Script(Op.GetPushOp(Encoding.UTF8.GetBytes(coinbaseString))).ToBytes();
 
