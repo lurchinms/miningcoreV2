8e807614bd904136d970d10de3753948e6f5b142
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2026-02-04 17:43:56 -0600

    Fix "minerEffort" calculation

--- src/Miningcore/Payments/PayoutManager.cs
+++ src/Miningcore/Payments/PayoutManager.cs
@@ -258,9 +258,8 @@ public class PayoutManager : BackgroundService
 
 	var miner = block.Miner;
 
-        // get last block for pool
-        var lastBlock = await cf.Run(con => blockRepo.GetMinerBlockBeforeAsync(con, poolConfig.Id, miner, new[]
-        {
+        // get last block for pool even for "MinerEffort". We use the same method as pool effort because adding miner address in the equation will just create an overlap in the final calculationMore actions
+        var lastBlock = await cf.Run(con => blockRepo.GetBlockBeforeAsync(con, poolConfig.Id, new[]        {
             BlockStatus.Confirmed,
             BlockStatus.Orphaned,
             BlockStatus.Pending,
--- src/Miningcore/Persistence/Postgres/Repositories/BlockRepository.cs
+++ src/Miningcore/Persistence/Postgres/Repositories/BlockRepository.cs
@@ -133,7 +133,9 @@ public class BlockRepository : IBlockRepository
             .FirstOrDefault();
     }
     
+
     public async Task<uint> GetBlockBeforeCountAsync(IDbConnection con, string poolId, BlockStatus[] status, DateTime before)
+
     {
         const string query = @"SELECT * FROM blocks WHERE poolid = @poolid AND status = ANY(@status) AND created < @before";
         
@@ -193,20 +195,6 @@ public class BlockRepository : IBlockRepository
         return con.ExecuteScalarAsync<DateTime?>(new CommandDefinition(query, new { poolId, address }, cancellationToken: ct));
     }
 
-    public async Task<Block> GetBlockByPoolHeightAndTypeAsync(IDbConnection con, string poolId, long height, string type)
-    {
-        const string query = @"SELECT * FROM blocks WHERE poolid = @poolId AND blockheight = @height AND type = @type";
-
-        return (await con.QueryAsync<Entities.Block>(query, new
-        { 
-            poolId,
-            height,
-            type
-        }))
-            .Select(mapper.Map<Block>)
-            .FirstOrDefault();
-    }
-    
     public async Task<uint> GetPoolDuplicateBlockCountByPoolHeightNoTypeAndStatusAsync(IDbConnection con, string poolId, long height, BlockStatus[] status)
     {
         const string query = @"SELECT COUNT(id) FROM blocks WHERE poolid = @poolId AND blockheight = @height AND status = ANY(@status)";
